bfb257c148808b081ee330bef45b8ea1fcb3c9c6
Edgar Vardanyan <e.vardan4an@gmail.com>
2022-09-20 18:15:03 +0400

    first solution

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -4,6 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListCreator {
-    public ArrayList<String> createArrayList(List<String> sourceList) {
+    public  ArrayList<String> createArrayList(List<String> sourceList) {
+        ArrayList<String> result = new ArrayList<>();
+
+        for (int i = 1; i < sourceList.size(); i++) {
+            if ( (i + 1) % 3 == 0) {
+                result.add(sourceList.get(i));
+                result.add(sourceList.get(i));
+            }
+        }
+
+        return result;
     }
+
 }
